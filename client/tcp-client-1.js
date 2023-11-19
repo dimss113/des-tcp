de8ce86347c7f61b0abb0c14ba1137d928d0335a
Dimas Fadilah Akbar <5025211010@mhs.its.ac.id>
2023-11-20 00:10:53 +0700

    feat: clien

--- client/tcp-client-1.js
+++ client/tcp-client-1.js
@@ -7,30 +7,30 @@ const port = 8888;
 //=================================================
 
 // Create the socket client.
-const client1 = new net.Socket();
+const client = new net.Socket();
 
 // Connect to the server on the configured port
-client1.connect(port, function () {
+client.connect(port, function () {
     // Log when the connection is established
     console.log(`Client 1: Connected to server on port ${port}`);
 
     // Try to send data to the server
-    client1.write('Hi from the client 1');
+    client.write('Hi from the client 1');
 });
 
 // Handle data coming from the server
-client1.on('data', function (data) {
+client.on('data', function (data) {
     let decryptedMessage = DecryptMessage(`${data}`, "test1234");
     console.log(`Client 1 received from server: ${decryptedMessage}`);
 });
 
 // Handle connection close
-client1.on('close', function () {
+client.on('close', function () {
     console.log('Client 1: Connection Closed');
 });
 
 // Handle error
-client1.on('error', function (error) {
+client.on('error', function (error) {
     console.error(`Connection Error ${error}`);
 });
 
@@ -42,5 +42,5 @@ const rl = readline.createInterface({
 
 // Read input from the user and send it to the server
 rl.on('line', function (input) {
-    client1.write(input);
+    client.write(input);
 });
